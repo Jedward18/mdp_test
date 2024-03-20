3b38d4d23388e31a635a28c6952eaafbd4cb106a
John Edward Lachica <johnedwardlachica30@gmail.com>
2024-03-20 14:39:50 +0100

    operazione

---
+++ operazioni.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar 20 14:36:59 2024
+
+@author: lachi
+"""
+def somma(a,b) -> int:
+    
+    res = a + b;
+    return res
+
+
+def moltiplicazione(a,b) -> int:
+    
+    res = a * b;
+    return res
+
+
+def divisione(a,b) ->int:
+    
+    res = a/b;
+    return res
